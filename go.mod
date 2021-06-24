69db3104b984287bcf547fccef01deee45039f20
Sergey Boldyrev <boldyrev@neurus.ru>
2021-06-24 17:24:00 +0000

    changing init new seq functionality

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/NotInTheSubject/prowler v0.0.0-20210624061802-2216d8d626d1
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 )
--- main.go
+++ main.go
@@ -2,48 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net/http"
 	"time"
 
 	"io"
-	"net/http/httputil"
 	"net/url"
 
 	"github.com/NotInTheSubject/prowler"
 	"github.com/sirupsen/logrus"
-	// mods "github.com/NotInTheSubject/crips/pkg/modifiers"
+	"github.com/satori/go.uuid"
 )
 
-// https://www.google.com/imghp#
 // access_token=AQAAAABVeLR8AAcrNJxQV_sR8kbIv9OtUCTzZ_4&token_type=bearer&expires_in=31536000
-
-// type sequence = []mods.RequestProducer
-
-func Requests() {
-	req, err := NewRequestProvider().CreateFolder("/raw", []string{})
-	if err != nil {
-		logrus.Fatalf("Request-CreatingRequest: \n%+v", err)
-	}
-	bytesReq, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		logrus.Fatalf("Request-DumpRequest: \n%+v", err)
-	}
-	logrus.Infof("Request: \n%+v", string(bytesReq))
-	client := &http.Client{
-		Timeout: time.Second * 30,
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		logrus.Fatalf("Request-Do: %+v", err)
-	}
-	bytesRes, err := httputil.DumpResponse(res, true)
-	if err != nil {
-		logrus.Fatalf("Request-DumpResponse: %+v", err)
-	}
-	logrus.Infof("Response: \n%+v", string(bytesRes))
-	logrus.Info("Success")
-}
 
 type ApiRequestProvider struct {
 	startURL url.URL
@@ -128,30 +98,35 @@ type sequenceProducer struct {
 	rp         ApiRequestProvider
 }
 
-func (s sequenceProducer) GetRequest(resp *http.Response) (prowler.IdentifiedRequest, error) {
-	waiting := func() {<- time.After(5 * time.Second)}
+type wrapperSeqProd struct {
+	s *sequenceProducer
+}
+
+func (w wrapperSeqProd) GetRequest(resp *http.Response) (prowler.IdentifiedRequest, error) {
+	return w.s.GetRequest(resp)
+}
+
+func (s *sequenceProducer) GetRequest(resp *http.Response) (prowler.IdentifiedRequest, error) {
+	// waiting := func() { <-time.After(5 * time.Second) }
 
 	if resp != nil {
-		if resp.StatusCode == 409 {
-			waiting()
-			s.progress--
-		} else if resp.StatusCode > 400 {
+
+		logrus.
+			WithField("resp-code", resp.StatusCode).
+			WithField("progress", s.progress).Info()
+
+		if resp.StatusCode > 400 {
 			return prowler.IdentifiedRequest{}, fmt.Errorf("unexpected status code (%v) in the response", resp.StatusCode)
+		} else {
+			s.progress++
 		}
 	}
-	defer func() { s.progress++ }()
 
 	if s.progress == 0 {
 		req, err := s.rp.CreateFolder(s.folderName, []string{})
 		return prowler.IdentifiedRequest{Request: req, Identifer: "creating folder"}, err
 	}
 	if s.progress == 1 {
-		destination := s.folderName + "newdest"
-		req, err := s.rp.MoveResource(s.folderName, destination, []string{})
-		s.folderName = destination
-		return prowler.IdentifiedRequest{Request: req, Identifer: "moving folder"}, err
-	}
-	if s.progress == 2 {
 		req, err := s.rp.DeleteFolder(s.folderName, []string{})
 		return prowler.IdentifiedRequest{Request: req, Identifer: "deleting folder"}, err
 	}
@@ -161,8 +136,13 @@ func (s sequenceProducer) GetRequest(resp *http.Response) (prowler.IdentifiedReq
 type requestSystem struct{}
 
 func (rs requestSystem) GetSequenceProducer() (prowler.SequenceProducer, error) {
-	folderName := fmt.Sprintf("/fuzztest%v", rand.Int())
-	return sequenceProducer{progress: 0, folderName: folderName, rp: NewRequestProvider()}, nil
+	
+	folderName := fmt.Sprintf("/fuzz-testfolder-%v", uuid.NewV4().String())
+	return wrapperSeqProd{&sequenceProducer{
+		progress:   0,
+		folderName: folderName,
+		rp:         NewRequestProvider(),
+	}}, nil
 }
 
 func main() {
